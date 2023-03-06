da0e4bac9d07830bea7021eda76378c5a20bea7d
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-03-06 20:06:29 +0200

    Add files via upload

---
+++ DateVariablesApp.java
@@ -0,0 +1,33 @@
+
+
+public class DateVariablesApp {
+    public static void main(String[]args){
+        
+        int curentYear = 2023;
+        int curentMonth = 03;
+        int curentDay =04;
+
+        System.out.print("Today is: ");
+        System.out.print(curentDay);
+        System.out.print("-" );
+        System.out.print(curentMonth);
+        System.out.print("-");
+        System.out.print(curentYear);;
+        
+
+
+
+    }
+}
+
+ /* 
+ 
+ diferences between print() si println() - 
+ aceasta este o functie de afisare pe ecran a informatiei dar in cazul 2 ,
+ imediat dupa afisarea informatiei dintre parantezele rotunde, cursorul va trece automat din rand nou. 
+
+ simple data types in java - int, boolean, byte short, long, float, double, char and boolean. 
+
+ */ 
+
+
