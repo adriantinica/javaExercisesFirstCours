7b9c5096c0ffd4d49fbc11cd87db87164d795e0b
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-03-13 20:05:41 +0200

    Add files via upload
    Simple if /else exercise in JAVA

---
+++ FirstFlowControlApp.java
@@ -0,0 +1,13 @@
+public class FirstFlowControlApp{
+    public static void main(String[] args) {
+        
+        int temperature = 18;
+
+        if ( temperature >= 18) {
+            System.out.println("It is warm in the building");
+        }
+        else{
+            System.out.println("It is cold in the building");
+        }
+    }
+}
