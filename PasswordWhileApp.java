89b1a1a29cd4542580a14fbc4f4841f12f472d45
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-03-25 09:31:51 +0200

    Add files via upload

--- PasswordWhileApp.java
+++ PasswordWhileApp.java
@@ -1,19 +1,47 @@
 import java.util.Scanner;
 public class PasswordWhileApp {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
     
         Scanner in = new Scanner (System.in);
         final int secretPIN = 1234;
         int pin = 0;
-        do{
+        byte increment = 0;
+
+
+        // do{
+        //     System.out.println("Enter PIN: ");
+        //     pin = in.nextInt();
+        // } while (pin != secretPIN);
+
+        while ( increment <= 2 ){
             System.out.println("Enter PIN: ");
             pin = in.nextInt();
-        } while (pin != secretPIN);
-        
-        if ( pin == secretPIN ) {
-            System.out.println("Login Successfully !!!");
+
+            if ( pin == secretPIN ) {
+                System.out.println("Login Successfully !!!");
+                break;
+            }
+
+           
+            while ( pin != secretPIN ){
+                System.out.println("WRONG PIN");
+                
+                increment ++;
+                
+                
+                if ( increment == 3 ) {
+                    System.out.println("WARNING !!!");
+                    System.out.println("Acount is blocked; Maximum tries 3 !!! ");
+                }  break ;      
+         
+            }  
+           
+
         }
-
-
+        
+ 
     }   
 }
